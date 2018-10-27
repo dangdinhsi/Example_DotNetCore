5cab37a5a06e4827083987391d7936bb1a23d476
dangdinhsi <siddd00474@fpt.edu.vn>
2018-10-27 16:04:09 +0700

    example .net core the first day

---
+++ MyNetCoreMVC/Data/MyNetCoreMVCContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyNetCoreMVC.Models
+{
+    public class MyNetCoreMVCContext : DbContext
+    {
+        public MyNetCoreMVCContext (DbContextOptions<MyNetCoreMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MyNetCoreMVC.Models.Product> Product { get; set; }
+    }
+}
